58629c20b1ba664e96c48a349c8cff8fd517c954
emykiz <gideoneffiong09@gmail.com>
2022-12-21 15:22:16 +0100

    viewpost

--- frontend/post.js
+++ frontend/post.js
@@ -1,254 +1,317 @@
 const data = [
-    {
-        head1:"Admission into Emediong",
-        head2:"university of emediong",
-        head3:"we bring good tiding",
-        news:"seriously i am good"
-    },
-    {
-      head1:"Admission into peter",
-      head2:"university of peter",
-      head3:"we bring good peter",
-      news:"seriously i am peter"
-    },
-    {
-      head1:"Admission into john",
-      head2:"university of johm",
-      head3:"we bring good johm",
-      news:"seriously i am johm"
-    },
-    {
-      head1:"Admission into game",
-      head2:"university of game",
-      head3:"we bring good game",
-      news:"seriously i am game"
-    },
-    {
-      head1:"Admission into destiny",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
+  {
       head1:"Admission into Emediong",
       head2:"university of emediong",
       head3:"we bring good tiding",
-      news:"seriously i am good"
-    },
-    {
-      head1:"Admission into peter",
-      head2:"university of peter",
-      head3:"we bring good peter",
-      news:"seriously i am peter"
-    },
-    {
+      news:"seriously i am good bghv,jhlgj;kklo'po;ljhghjkl;u/o;lhrtyuiop;kljwrtyuioljkjgfdfgklh.,bvcgdhjkjlhvhgcfsiuufytdrrtyuiyftdrsdfghjklkjhvgcfdghjkl;ljkhgfdsdtdyuilk,nmbvcxv bnmbvcxfdtyukklhjcgxftdyuioihgfdffghjklkjhjghf",
+      image:"Ellipse 7.png"
+  },
+  {
+    head1:"Admission into peter",
+    head2:"university of peter",
+    head3:"we bring good peter",
+    news:"seriously i am peter",
+    image:"ipayBTC logo.jpg"
+
+  },
+  {
+    head1:"Admission into john",
+    head2:"university of johm",
+    head3:"we bring good johm",
+    news:"seriously i am johm",
+    image:"MGH Group Logo.png"
+  },
+  {
+    head1:"Admission into game",
+    head2:"university of game",
+    head3:"we bring good game",
+    news:"seriously i am game",
+    image:"pencil.png"
+  },
+  {
+    head1:"Admission into destiny",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into Emediong",
+    head2:"university of emediong",
+    head3:"we bring good tiding",
+    news:"seriously i am good",
+    image:"pencil.png"
+  },
+  {
+    head1:"Admission into peter",
+    head2:"university of peter",
+    head3:"we bring good peter",
+    news:"seriously i am peter",
+    image:"MGH Group Logo.png"
+  },
+  {
+    head1:"Admission into john",
+    head2:"university of johm",
+    head3:"we bring good johm",
+    news:"seriously i am johm",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into precious",
+    head2:"university of joy",
+    head3:"we bring good precious",
+    news:"seriously i am precious",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into king",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"Ellipse 7.png"
+  },
+  {
+    head1:"Admission into file",
+    head2:"file oh",
+    head3:"we bring good file",
+    news:"seriously i am file",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into john",
+    head2:"university of johm",
+    head3:"we bring good johm",
+    news:"seriously i am johm",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into Emediong",
+    head2:"university of emediong",
+    head3:"we bring good tiding",
+    news:"seriously i am good",
+    image:"MGH Group Logo.png"
+
+  },
+  {
       head1:"Admission into john",
       head2:"university of johm",
       head3:"we bring good johm",
-      news:"seriously i am johm"
-    },
-    {
-      head1:"Admission into precious",
-      head2:"university of joy",
-      head3:"we bring good precious",
-      news:"seriously i am precious"
-    },
-    {
-      head1:"Admission into king",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into file",
-      head2:"file oh",
-      head3:"we bring good file",
-      news:"seriously i am file"
-    },
-    {
-      head1:"Admission into john",
-      head2:"university of johm",
-      head3:"we bring good johm",
-      news:"seriously i am johm"
-    },
-    {
-      head1:"Admission into Emediong",
-      head2:"university of emediong",
-      head3:"we bring good tiding",
-      news:"seriously i am good"
-    },
-    {
-        head1:"Admission into john",
-        head2:"university of johm",
-        head3:"we bring good johm",
-        news:"seriously i am johm"
-    },
-    {
-      head1:"Admission into game",
-      head2:"university of game",
-      head3:"we bring good game",
-      news:"seriously i am game"
-    },
-    {
-      head1:"Admission into john",
-      head2:"university of johm",
-      head3:"we bring good johm",
-      news:"seriously i am johm"
-    },
-    {
-      head1:"Admission into file",
-      head2:"file oh",
-      head3:"we bring good file",
-      news:"seriously i am file"
-    },
-    {
-      head1:"Admission into precious",
-      head2:"university of joy",
-      head3:"we bring good precious",
-      news:"seriously i am precious"
-    },
-    {
-      head1:"Admission into game",
-      head2:"university of game",
-      head3:"we bring good game",
-      news:"seriously i am game"
-    },
-    {
-      head1:"Admission into destiny",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into revi ",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into deside",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into revi ",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into contr",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into destiny",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into peter",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into wgy",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into dgruioo",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into believe",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into gather",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into dgruioo",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    },
-    {
-      head1:"Admission into wgy",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into dgruioo",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-    {
-      head1:"Admission into believe",
-      head2:"university of destuim",
-      head3:"we bring good destomu",
-      news:"seriously i am ghggj"
-    }, 
-  ];
-  const datas =
-  window.booksPerPage = 5;
-  window.curr = 1;
-  data.slice(0,booksPerPage).forEach((obj) => {
-    document.getElementById("data").innerHTML += `<h1 class="head1">${obj.head1}</h1>
-                                                      <h1 class="head2">${obj.head2}</h1>
-                                                      <h6 class="head3">${obj.head3}</h6>
-                                                      <div class="news-flex">
-                                                          <div>
-  
-                                                          </div>
-                                                          <p class="news-deatails">
-                                                              ${obj.news}
-                                                              <span>[continue...]</span>
-                                                          </p>`;
+      news:"seriously i am johm",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into game",
+    head2:"university of game",
+    head3:"we bring good game",
+    news:"seriously i am game",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into john",
+    head2:"university of johm",
+    head3:"we bring good johm",
+    news:"seriously i am johm",
+    image:"Ellipse 7.png"
+
+  },
+  {
+    head1:"Admission into file",
+    head2:"file oh",
+    head3:"we bring good file",
+    news:"seriously i am file",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into precious",
+    head2:"university of joy",
+    head3:"we bring good precious",
+    news:"seriously i am precious",
+    image:"MGH Group Logo.png"
+  },
+  {
+    head1:"Admission into game",
+    head2:"university of game",
+    head3:"we bring good game",
+    news:"seriously i am game",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into destiny",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+  },
+  {
+    head1:"Admission into revi ",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into deside",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into revi ",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into contr",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into destiny",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into peter",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into wgy",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into dgruioo",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into believe",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into gather",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into dgruioo",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  },
+  {
+    head1:"Admission into wgy",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into dgruioo",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+  {
+    head1:"Admission into believe",
+    head2:"university of destuim",
+    head3:"we bring good destomu",
+    news:"seriously i am ghggj",
+    image:"MGH Group Logo.png"
+
+  }, 
+];
+const datas =
+window.booksPerPage = 5;
+window.curr = 1;
+data.slice(0,booksPerPage).forEach((obj) => {
+  document.getElementById("data").innerHTML += `<h1 class="head1">${obj.head1}</h1>
+                                                    <h1 class="head2">${obj.head2}</h1>
+                                                    <h6 class="head3">${obj.head3}</h6>
+                                                    <div class="news-flex">
+                                                        <div class="object-fit">
+                                                            <img src="images/${obj.image}">
+                                                        </div>
+                                                        <p class="news-deatails">
+                                                            ${obj.news}
+                                                            <a href="./viewpost.html"><span>[continue...]</span></a>
+                                                        </p>
+                                                    </div>`;
+                                                    
+});
+function changeData(index) {
+  document.querySelectorAll("button")[window.curr].classList.remove("active");
+  document.querySelectorAll("button")[index].classList.add("active");
+  let showData = data.slice((index-1)*booksPerPage,index*booksPerPage);
+  document.getElementById("data").innerHTML = "";
+  showData.forEach((obj) => {
+      document.getElementById("data").innerHTML += `<h1 class="head1">${obj.head1}</h1>
+      <h1 class="head2">${obj.head2}</h1>
+      <h6 class="head3">${obj.head3}</h6>
+      <div class="news-flex">
+          <div class="object-fit>
+            <img src="images/${obj.image}">
+          </div>
+          <p class="news-deatails">
+              ${obj.news}
+              <span>[continue...]</span>
+          </p>
+        </div>`;
   });
-  function changeData(index) {
-    document.querySelectorAll("button")[window.curr].classList.remove("active");
-    document.querySelectorAll("button")[index].classList.add("active");
-    let showData = data.slice((index-1)*booksPerPage,index*booksPerPage);
-    document.getElementById("data").innerHTML = "";
-    showData.forEach((obj) => {
-        document.getElementById("data").innerHTML += `<h1 class="head1">${obj.head1}</h1>
-        <h1 class="head2">${obj.head2}</h1>
-        <h6 class="head3">${obj.head3}</h6>
-        <div class="news-flex">
-            <div>
-  
-            </div>
-            <p class="news-deatails">
-                ${obj.news}
-                <span>[continue...]</span>
-            </p>`;;
-    });
-    window.curr = index;
+  window.curr = index;
+}
+
+function next() {
+  if (window.curr < 10) {
+      changeData(window.curr+1);
   }
-  
-  function next() {
-    if (window.curr < 10) {
-        changeData(window.curr+1);
-    }
+}
+
+function prev() {
+  if (window.curr > 1) {
+      changeData(window.curr-1);
   }
-  
-  function prev() {
-    if (window.curr > 1) {
-        changeData(window.curr-1);
-    }
-  }
+}
--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,4 @@
 
-
-
-// const { default: axios } = require("axios");
-
 const menuIcon = document.querySelector(".menu-icon");
 const lines = document.querySelectorAll(".menu-icon div");
 console.log(menuIcon);
@@ -18,95 +14,6 @@ menuIcon.addEventListener("click", () => {
   // hide.style.visibility = 'visible'
 
 });
-
-
-
-
-
-
-
-
-
-
-
-var header;
-header = document.querySelector('header');
-console.log(header);
-var navitem;
-navitem = document.querySelectorAll("nav > ul > li a");
-var count = 0;
-var image_url = [
-    // 'back.jpg',
-    'Ellipse 7.png',
-    //'back3.jpg',
-    'pencil.png',
-    // 'back5.jpg',
-    'MGH Group Logo.png',
-    // 'back7.jpg',
-];
-function checkbgcolor(count) {
-  if (count === 0) {
-      navitem.forEach(function (a) { return a.style.color = "rgb(116,79,99)"; });
-     // menu.style.background = "rgba(96,125,139,0.49)";
-  }
-  else {
-      navitem.forEach(function (a) { return a.style.color = "black"; });
-     // menu.style.background = "none";
-  }
-}
-var getdot = document.querySelectorAll('.dot_move > .dot');
-console.log(getdot);
-var nextDom = document.querySelector('.move > .next');
-var prevDom = document.querySelector('.move > .prev');
-var nexts = function () {
-    if (window.innerWidth >= 350) {
-        getdot.forEach(function (element) {
-            element.classList.remove("active");
-        }); //making all the dot boxes unactive
-        if (count < image_url.length - 1) {
-            ++count;
-            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
-            getdot[count].classList.add("active"); //making the position of the below dot active when the image is displayed in that position
-            checkbgcolor(count); //check the background color
-        }
-        else {
-            count = 0;
-            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
-            getdot[count].classList.add("active");
-            checkbgcolor(count);
-        }
-    }
-    else {
-        header.style.backgroundImage = "none";
-    }
-};
-var prevs = function () {
-    if (window.innerWidth >= 350) {
-        getdot.forEach(function (element) {
-            element.classList.remove("active");
-        }); //making all the dot boxes unactive
-        if (count > 0) {
-            --count;
-            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
-            getdot[count].classList.add("active");
-            checkbgcolor(count);
-        }
-        else {
-            count = image_url.length - 1;
-            header.style.backgroundImage = "url(img/".concat(image_url[count], ")");
-            getdot[count].classList.add("active");
-            checkbgcolor(count);
-        }
-    }
-    else {
-        header.style.backgroundImage = "none";
-    }
-};
-var inter = setInterval(nexts, 1000);
-window.innerWidth <= 350 ? clearInterval(inter) : null; //stop the interval on smaller screen
-prevDom === null || prevDom === void 0 ? void 0 : prevDom.addEventListener('click', prevs);
-nextDom === null || nextDom === void 0 ? void 0 : nextDom.addEventListener('click', nexts);
-
 
 
 let slidePosition = 0;
